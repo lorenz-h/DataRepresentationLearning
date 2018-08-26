1c0b44f0d3c6a38764f303a844ae415f165dcc4d
Lorenz Hetzel <lorenz@hetzel.io>
2018-08-26 16:55:48 +0200

    Added documentation to the input pipeline

--- ImitationDuckie/ID_Input_Pipeline.py
+++ ImitationDuckie/ID_Input_Pipeline.py
@@ -6,6 +6,11 @@ field_defaults = [[0.0], [""]]
 
 
 def _parse_csv_line(line):
+    """
+    This reads a csv file.
+    :param line: The line of the csv file to read.
+    :return:
+    """
     fields = tf.decode_csv(line, field_defaults)
     features = dict(zip(columns, fields))
     label = features.pop("angularz")
@@ -14,6 +19,12 @@ def _parse_csv_line(line):
 
 
 def _parse_image(image_path, label):
+    """
+    This maps the image path in the csv file to the acutal image file
+    :param image_path: the path to the image
+    :param label: the label for the image
+    :return: the image array and the corrsponding label
+    """
     img_file = tf.read_file(image_path)
     img_decoded = tf.image.decode_image(img_file, channels=1)
     img_decoded = tf.cast(img_decoded / 255, dtype=tf.float32)
@@ -21,6 +32,12 @@ def _parse_image(image_path, label):
 
 
 def create_dataset(batch_size, csv_file):
+    """
+    This creates a tensorflow dataset object.
+    :param batch_size: the batch size to use
+    :param csv_file: the csv file to read the dataset from
+    :return:
+    """
     ds = tf.data.TextLineDataset(csv_file)
     ds = ds.map(_parse_csv_line, num_parallel_calls=4)
     ds = ds.map(_parse_image, num_parallel_calls=16)
